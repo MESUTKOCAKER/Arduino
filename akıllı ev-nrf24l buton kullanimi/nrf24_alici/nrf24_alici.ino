2789f032fddbf979e9590e7d46924c875c9bd395
MESUT KOÇAKER <mesut0723@gmail.com>
2020-01-07 00:11:26 +0300

    Add files via upload

---
+++ akıllı ev-nrf24l buton kullanimi/nrf24_alici/nrf24_alici.ino
@@ -0,0 +1,37 @@
+//şu anki kodlar buton basılınca sayının sonuna 1
+//butona 2. kez basınca 0 olan kodları içerir
+#include <SPI.h>
+#include <nRF24L01p.h>
+
+String message;
+
+nRF24L01p receiver(10,9);
+
+void setup()
+{
+     delay(150);
+     SPI.begin();
+     SPI.setBitOrder(MSBFIRST);
+     receiver.channel(90);
+     receiver.RXaddress("tiny");
+     receiver.init();
+     Serial.begin(9600);
+}
+
+void loop()
+{
+     if (receiver.available())
+     {
+          receiver.read();
+          receiver.rxPL(message);
+          if(message != "")
+          {
+               Serial.println(message);
+               message = "";
+          }
+          delay(100);
+     }
+ }
+
+
+ 
---
+++ akıllı ev-nrf24l buton kullanimi/nrf24_verici/nrf24_verici.ino
@@ -0,0 +1,55 @@
+//şu anki kodlar buton basılınca sayının sonuna 1
+//butona 2. kez basınca 0 olan kodları içerir
+#include <nRF24L01p.h>
+#include <SPI.h>
+
+#define CE_PIN 9
+#define CSN_PIN 10
+
+nRF24L01p transmitter(10,9);
+
+//const uint64_t pipe = 0xE8E8F0F0E1LL;
+int sayac = 1;
+String mesaj = "AkilliEv";
+
+//potasiometre
+int aci = 98;
+//potasiometre
+
+
+/*verici buton*/
+const int buttonPin = 2;//buton
+int lambadurum = 0;
+/*verici buton*/
+
+
+void setup()
+{
+     SPI.begin();
+     SPI.setBitOrder(MSBFIRST);
+     transmitter.channel(90);
+     transmitter.TXaddress("tiny");
+     transmitter.init();
+     pinMode(buttonPin, INPUT);//buton
+}
+
+void loop()
+{
+  if(digitalRead(buttonPin) == 1)
+  {
+    if(lambadurum == 1)
+      lambadurum = 0;
+    else
+      lambadurum = 1;
+      delay(24);
+  }
+    mesaj = String(aci) + lambadurum;
+
+
+    
+     transmitter.txPL(mesaj);
+     transmitter.send(FAST);
+     delay(100);
+     sayac += 2;
+}
+

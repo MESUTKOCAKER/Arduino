56271be054cab83190dbb3f4aa02ed391654d18b
MESUT KOÇAKER <mesut0723@gmail.com>
2020-01-07 00:12:50 +0300

    Add files via upload

---
+++ akıllı ev-nrf24l buton ile led yakma/nrf24_alici/nrf24_alici.ino
@@ -0,0 +1,43 @@
+//şu anki kodlar buton basılınca sayının sonuna 1
+//butona 2. kez basınca 0 olan kodları içerir
+#include <SPI.h>
+#include <nRF24L01p.h>
+
+int message;
+
+nRF24L01p receiver(10,9);
+
+
+int led = 7;
+void setup()
+{
+     delay(150);
+     SPI.begin();
+     SPI.setBitOrder(MSBFIRST);
+     receiver.channel(90);
+     receiver.RXaddress("tiny");
+     receiver.init();
+     Serial.begin(9600);
+     pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+     if (receiver.available())
+     {
+          receiver.read();
+          receiver.rxPL(message);
+          if(message != "")
+          {
+               Serial.println(message);
+               int ledDurum = (int)message;
+               ledDurum = ledDurum %10;
+               digitalWrite(led, ledDurum);
+               message = "";
+          }
+          delay(100);
+     }
+ }
+
+
+ 

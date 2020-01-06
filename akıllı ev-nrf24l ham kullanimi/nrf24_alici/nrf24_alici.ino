26b0e49b3166dc99c3e7b066039a5f5d3c07d036
MESUT KOÇAKER <mesut0723@gmail.com>
2020-01-07 00:08:31 +0300

    Add files via upload

---
+++ akıllı ev-nrf24l ham kullanimi/nrf24_alici/nrf24_alici.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <nRF24L01p.h>
+
+String message;
+
+nRF24L01p receiver(10,9);
+
+void setup()
+{
+     delay(150);
+     SPI.begin();
+     SPI.setBitOrder(MSBFIRST);
+     receiver.channel(90);
+     receiver.RXaddress("tiny");
+     receiver.init();
+     Serial.begin(9600);
+}
+
+void loop()
+{
+     if (receiver.available())
+     {
+          receiver.read();
+          receiver.rxPL(message);
+          if(message != "")
+          {
+               Serial.println(message);
+               message = "";
+          }
+          delay(100);
+     }
+ }
+
+
+ 
---
+++ akıllı ev-nrf24l ham kullanimi/nrf24_verici/nrf24_verici.ino
@@ -0,0 +1,29 @@
+#include <nRF24L01p.h>
+#include <SPI.h>
+
+#define CE_PIN 9
+#define CSN_PIN 10
+
+nRF24L01p transmitter(10,9);
+
+//const uint64_t pipe = 0xE8E8F0F0E1LL;
+int sayac = 1;
+String mesaj = "AkilliEv";
+
+void setup()
+{
+     SPI.begin();
+     SPI.setBitOrder(MSBFIRST);
+     transmitter.channel(90);
+     transmitter.TXaddress("tiny");
+     transmitter.init();
+}
+
+void loop()
+{
+     transmitter.txPL(mesaj + (sayac));
+     transmitter.send(FAST);
+     delay(500);
+     sayac += 2;
+}
+
